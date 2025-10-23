460ab44520e5c0f3d7fd1d5912e705e4ea003f55
MauroAlmonacid <canepheler@gmail.com>
2025-10-23 01:03:12 -0300

    boton amburgesa responsive

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,31 +1,32 @@
+// RUTA: src/components/layout/Header.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCart } from '../../contexts/CartContext';
+// Asegúrate de tener importado 'bootstrap-icons/font/bootstrap-icons.css' en tu main.jsx o index.html
 
-// Recibimos la función `toggleSidebar` como una prop desde MainLayout
 function Header({ toggleSidebar }) {
   const { itemCount } = useCart();
 
   return (
     <header className="header">
       <div className="header-inner">
-        {/* Este es el nuevo botón de hamburguesa */}
-        <button className="hamburger-menu" onClick={toggleSidebar}>
-          <i className="fa-solid fa-bars"></i>
+        {/* Usando el ícono de Bootstrap */}
+        <button className="hamburger-menu" onClick={toggleSidebar} aria-label="Abrir menú">
+          <i className="bi bi-list"></i> {/* Ícono de Bootstrap */}
         </button>
 
         <h1 className="logo"><Link to="/">Andromeda's Inn Shop</Link></h1>
-        
+
         <div className="search-bar">
           <form action="#" method="get">
             <input type="text" name="search" placeholder="Buscar productos..." />
             <button type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
           </form>
         </div>
-        
+
         <div className="cart-container">
           <Link to="/carrito">
-            <i className="fa-solid fa-cart-shopping"></i> 
+            <i className="fa-solid fa-cart-shopping"></i> {/* Mantienes Font Awesome aquí o cambias a bi-cart */}
             <span className="cart-text"> Carrito ({itemCount})</span>
           </Link>
         </div>
