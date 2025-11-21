692a99bb82965f44c7748fef94e1b3973969aec2
RETR0NACH <ichaconaedo@gmail.com>
2025-11-21 17:05:30 -0300

    url

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL || 'https://andromeda-backend-vqcx.onrender.com/api/v1';
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1';
 
 const api = axios.create({
   baseURL: apiUrl,
