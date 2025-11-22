47ba6b8fad5edb7797a95fcc04ec4ea717f1c6ff
RETR0NACH <ichaconaedo@gmail.com>
2025-11-22 13:31:25 -0300

    fix ruta

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL || 'https://andromeda-backend-vqcx.onrender.com/api/v1';
+const apiUrl = import.meta.env.VITE_API_URL || 'https://andromeda-backend-vqcx.onrender.com/api/v1/productos';
 
 const api = axios.create({
   baseURL: apiUrl,
