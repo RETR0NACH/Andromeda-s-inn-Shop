21ac799916bcb73af7b4c8db7304df2b629d93e6
MauroAlmonacid <canepheler@gmail.com>
2025-11-22 23:19:22 -0300

    fix

--- src/config/api.js
+++ src/config/api.js
@@ -1,20 +1,9 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL || 'https://andromeda-backend-vqcx.onrender.com/api/v1/productos';
+const apiUrl = import.meta.env.VITE_API_URL || 'https://andromeda-backend-vqcx.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: apiUrl,
 });
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default api;
