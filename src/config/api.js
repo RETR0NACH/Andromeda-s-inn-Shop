73ac2cf6d4af86c09b3bd6704a70be98945145f7
RETR0NACH <ichaconaedo@gmail.com>
2025-11-23 01:28:14 -0300

    fix definitivo en api

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL || 'https://andromeda-backend-vqcx.onrender.com/api/v1';
+const apiUrl = 'https://backen-end.onrender.com/api/v1'; 
 
 const api = axios.create({
   baseURL: apiUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default api;
